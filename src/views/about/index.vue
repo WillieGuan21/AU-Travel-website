7eea9ee9cbd05f907b76455905fe6629792c57cd
WillieGuan21 <62228047+WillieGuan21@users.noreply.github.com>
2022-05-11 21:38:36 +0800

    Update index.vue

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -3,14 +3,8 @@
     <div class="grid grid-cols-1 gap-4 lg:grid-cols-3">
       <div>
         <div class="py-10 text-2xl">About Us</div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque iaculis consectetur dolor,
-        at pellentesque leo rutrum porta. Sed interdum justo vel nisi sagittis elementum. Nullam
-        nunc est, efficitur at mauris et, fringilla aliquam risus. Praesent faucibus enim nec
-        eleifend rutrum. Phasellus vel odio vitae urna pharetra tincidunt pretium quis ligula.
-        Quisque id varius dui, eu sodales orci. Duis cursus metus non ornare viverra. Donec non
-        tellus facilisis, aliquam enim eu, euismod leo. Aenean varius nunc eleifend nunc feugiat
-        consequat. Proin nec scelerisque lacus, ut malesuada turpis. Integer vulputate purus iaculis
-        convallis facilisis.
+        Hi, we are love travel couple, six years age, we meet in Sydney Australia, at that time, we are workingholiday in there, and we have pre-wedding photes in Perth.
+         We really love the place in Australia, also we traveling any country in the world. We want to recode the scenery and experience, keep it looking and moving forward.
       </div>
       <img class="col-span-2" src="https://fakeimg.pl/1920x1080/" />
     </div>
